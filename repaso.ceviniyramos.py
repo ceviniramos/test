65d6ba8a4ce4630f434b22a926fe91b0346951a1
ceviniramos <agustinjeremias74@gmail.com>
2025-03-12 14:14:29 -0300

    Add files via upload

---
+++ repaso.ceviniyramos.py
@@ -0,0 +1,11 @@
+numeros = [1,2,30,4,51]
+for numero in numeros:
+    cuadrado = numero**2
+    print (f"el cuadrado de {numero} es {cuadrado}")
+print (numeros[4])
+
+nombre = "marcos"
+apellido = "MENDOZA"
+frace = "Hola esta es una frace"
+longitud=len(frace)
+print(longitud)
