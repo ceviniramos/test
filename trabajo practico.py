cd2043fa04bd536632c42f69ecdb0c5f0e92ebb4
ceviniramos <agustinjeremias74@gmail.com>
2025-03-25 13:20:21 -0300

    Add files via upload

---
+++ trabajo practico.py
@@ -0,0 +1,22 @@
+estudiantes = {
+    "Juan Pérez": {"edad": 17, "materias": ["Matemáticas", "Física"]},
+    "Ana Gómez": {"edad": 16, "materias": ["Química", "Historia"]},
+    "Pedro López": {"edad": 18, "materias": ["Biología", "Inglés"]}
+}
+def agregar ():
+    pass
+def mostrar():
+    pass
+op=0
+while (op!=6):
+    
+    match op:
+        case 1:
+            agregar()
+        case 2:
+            mostrar()
+        case 3:
+
+
+
+
